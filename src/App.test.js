551458395cf267232f045d9eaae640b154485d87
Karen Hughes <Karen@yahoo.com>
2024-09-12 15:17:09 +0100

    files

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
 test('renders VAT Calculator text', () => {
   render(<App />);
   const heading = screen.getByText(/VAT CALCULATOR/i);
